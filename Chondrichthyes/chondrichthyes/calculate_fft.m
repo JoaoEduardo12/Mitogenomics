d4f11664595ca14e1c98ad5560b940570b7e611e
João Eduardo Teixeira <joao.edu.t@hotmail.com>
2021-07-06 10:06:39 +0100

    Delete Chondrichthyes/chondrichthyes directory

--- Chondrichthyes/chondrichthyes/calculate_fft.m
+++
@@ -1,9 +0,0 @@
-function[seq] = calculate_fft(seq)
-% Input: Sequence in time domain
-% Output: Sequence in frequence domain
-% ------------------------------------
-% seq = calculate_fft(seq)
-seq = detrend(seq);
-NFFT=2^(nextpow2(length(seq))); % Next highest power of 2 greater or equal to length(x)
-seq = fft(seq,NFFT);
-end
